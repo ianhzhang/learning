29847f436156b4c56b83373f1dadf03994ab6d3b
Ian Zhang <izhang@extremenetworks.com>
2018-08-13 19:19:09 -0400

    update

---
+++ go/4_go_comp/src/2_send_rest/main.go
@@ -0,0 +1,7 @@
+// https://www.thepolyglotdeveloper.com/2017/07/consume-restful-api-endpoints-golang-application/
+
+// https://stackoverflow.com/questions/24455147/how-do-i-send-a-json-string-in-a-post-request-in-go
+
+// https://medium.com/@marcus.olsson/writing-a-go-client-for-your-restful-api-c193a2f4998c
+
+// http://polyglot.ninja/golang-making-http-requests/
