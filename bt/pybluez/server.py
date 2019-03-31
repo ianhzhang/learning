e79c03e258c2b6a8c387c76e3920a2942dc24a1e
Ian Zhang <ianhzhang@gmail.com>
2019-03-30 22:26:24 -0400

    server.py bd address

--- bt/pybluez/server.py
+++ bt/pybluez/server.py
@@ -5,13 +5,17 @@ Bluetooth using PyBluez (with Python 2).
 
 import bluetooth
 
-hostMACAddress = '00:1f:e1:dd:08:3d' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+# The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+hostMACAddress = '14:4F:8A:06:45:EA' 
+
 port = 3
 backlog = 1
 size = 1024
+
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.bind((hostMACAddress, port))
 s.listen(backlog)
+
 try:
     client, clientInfo = s.accept()
     while 1:
