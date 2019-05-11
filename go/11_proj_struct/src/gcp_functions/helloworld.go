26a20fa9e4446127fc459a8b5d5acaef95dc9dca
Ian Zhang <izhang@extremenetworks.com>
2019-05-10 20:24:41 -0400

    proj struct

---
+++ go/11_proj_struct/src/gcp_functions/helloworld.go
@@ -0,0 +1,9 @@
+package gcp_functions
+
+import (
+	"fmt"
+)
+
+func Hello() {
+	fmt.Println("Hello")
+}
---
+++ go/11_proj_struct/src/testmain/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"gcp_functions"
+)
+
+func main() {
+	fmt.Println("main")
+	gcp_functions.Hello()
+}
