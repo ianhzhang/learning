c6c8fce7eaeb6405ba61535ea0c37cfb5d43ebb3
Ian Zhang <ianhzhang@gmail.com>
2019-06-08 09:51:22 -0400

    work on bt, computer to computer

--- bt/pybluez/client.py
+++ bt/pybluez/client.py
@@ -1,6 +1,15 @@
 """
 A simple Python script to send messages to a sever over Bluetooth
 using PyBluez (with Python 2).
+
+Ian: When the port and MAC address match.  It works.
+When the client quit.   The server quits also.
+We don't need to have a connection setup before sending data.
+I see at beginning, there is a connection refused, I tried to use 
+computer to setup connection.  Then the error disappear even I close
+the connection.
+Start server first.
+I used python 2 to do it.
 """
 
 import bluetooth
@@ -8,7 +17,7 @@ import bluetooth
 # ncu 14:4F:8A:06:45:EA
 
 serverMACAddress = '00:1A:7D:DA:71:11'
-port = 24
+port = 20
 
 print("open socket")
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
