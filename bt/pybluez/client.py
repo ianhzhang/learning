92d6afd3a07c41e476184e479b84e329913adf70
Ian Zhang <ianhzhang@gmail.com>
2019-06-08 09:45:29 -0400

    bt

--- bt/pybluez/client.py
+++ bt/pybluez/client.py
@@ -5,13 +5,21 @@ using PyBluez (with Python 2).
 
 import bluetooth
 
+# ncu 14:4F:8A:06:45:EA
+
 serverMACAddress = '00:1A:7D:DA:71:11'
 port = 24
 
+print("open socket")
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+print("connect ....")
 s.connect((serverMACAddress, port))
+
 while 1:
+    print("please input:")
     text = raw_input() # Note change to the old (Python 2) raw_input
+    print("input:", text)
     if text == "quit":
     	break
     s.send(text)
